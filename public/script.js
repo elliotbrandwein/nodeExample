35d450fc964b3f623b75a6f9a4bd3d174dd81046
Elliot Brandwein <elliotbrandwein1@gmail.com>
2016-06-14 15:46:09 -0400

    added vey special button

--- public/script.js
+++ public/script.js
@@ -3,3 +3,22 @@ function special(){
 $("#face").fadeOut();
 $("#face").fadeIn()
 }
+function verySpecial()
+{
+		setInterval(function()
+		{ 
+			$('body').css ("background-color",getRandomColor());
+		}, 100);
+		document.getElementById("clickMe").disabled=true;
+}
+function getRandomColor()
+ {
+	var letters = '0123456789ABCDEF'.split('');
+	var color = '#';
+	for (var i = 0; i < 6; i++ )
+	{
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	x=false;
+	return color;
+}

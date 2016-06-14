b30bf97a27b9fa5d0498b172d9896f4adbce4665
Elliot Brandwein <elliotbrandwein1@gmail.com>
2016-06-14 12:20:00 -0400

    initial commit

---
+++ x/app.js
@@ -0,0 +1,24 @@
+
+var express = require('express');
+var app = express();
+var exphbs  = require('handlebars'); 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function(req, res) 
+{     
+	res.render('home'); 
+});
+app.get('/about', function(req, res) 
+{     
+	res.render('about'); 
+});
+app.get('/inspiration', function(req, res) 
+{     
+	res.render('inspiration'); 
+});
+
+app.listen(3000, function ()
+{   
+	console.log('Inspiration app listening on port 3000!'); 
+});
